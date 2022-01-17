9506fdecbc46647f1bd12affca73c30f77798d9d
circular-biscuit <mrdeb99@gmail.com>
2022-01-17 19:31:10 +0530

    code cleaned up

--- eas.js
+++ eas.js
@@ -1,11 +1,22 @@
 //declare column and row arrays
 const column = [];
 const row = [];
-
-//get container from html
-const container = document.querySelector('#container');
 // set size of grid
 let gridSize = 16;
+//query selects
+const container = document.querySelector('#container');
+const newGridButton = document.querySelector('#newGridButton');
+const clearGridButton = document.querySelector('#clearGridButton');
+const removeGridButton = document.querySelector('#removeGridButton');
+// const columnList = document.querySelectorAll('.column');
+const columnList = document.getElementsByClassName('column');
+// const rowList = document.querySelectorAll('.row');
+const rowList = document.getElementsByClassName('row');
+
+//event listeners
+newGridButton.addEventListener('click', newGrid);
+clearGridButton.addEventListener('click', clearGrid);
+removeGridButton.addEventListener('click', removeGrid);
 
 //create grid of divs
 function createGrid(gridSize){
@@ -22,32 +33,36 @@ function createGrid(gridSize){
     }
 }
 
-
-
 //add etch to hovered over grid square 
 function etch(){
     // this.style.backgroundColor = 'aquamarine';
     this.classList.add('etch');
 }
 
-//button click functionality added
-const newGridButton = document.querySelector('#newGridButton');
-newGridButton.addEventListener('click', newGrid);
-
-//button to initialize blank grid
-const clearGridButton = document.querySelector('#clearGridButton');
-clearGridButton.addEventListener('click', clearGrid)
-
 //initialize new grid with entered grid size
 function newGrid(){
-    gridSize = prompt('how many rows & columns would you like your grid to be (max 100)', '16')
-    createGrid(gridSize);
+    gridSize = prompt('how many rows & columns would you like your grid to be (max 100)');
+    if(gridSize !== null && gridSize<=100){
+        removeGrid();
+        createGrid(gridSize);
+    }
 }
 
 //initialize blank grid
 function clearGrid(){
-
+    console.log('i am a clear grid button');
+    for(let i=0; i<rowList.length; i++){
+        // rowList[i].classList.add('initialize');
+        rowList[i].style.backgroundColor = 'crimson';
+    }
 }
 
-//
+//remove grid
+function removeGrid(){
+    console.log('i am a remove grid button');
+    for(let i=0; i<rowList.length; i++){
+        rowList[i].remove();
+    }
+}
+
 createGrid(gridSize);
